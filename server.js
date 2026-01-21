53c695c3d7caf6583589f98d774f3b988e9e93fd
Mohammed <caadi2121@gmail.com>
2026-01-21 13:53:38 +0300

    fix: remove node_modules from repo

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
